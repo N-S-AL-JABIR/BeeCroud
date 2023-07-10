a1127ed84d04e2d94d9402441ee4efbc308851b0
Al Jabir <136284119+N-S-AL-JABIR@users.noreply.github.com>
2023-07-11 00:29:58 +0600

    Add files via upload

---
+++ 1101 - Sequence of Numbers and Sum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,x,y,min;
+    for (i = 1; i <= 100; i++) {
+        scanf("%d %d",&x,&y);
+    if (x<=0 || y<=0)
+    {
+        break;
+    }
+    else if (x>y)
+        {
+            min=x;x=y;y=min;
+        }
+    int sum=0;   
+    for (j = x; j <= y; j++){
+        sum+=j; 
+    printf("%d ",j);
+    }
+     printf("Sum=%d\n",sum);
+    }
+    return 0;
+}
